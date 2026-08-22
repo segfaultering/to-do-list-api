acb6aaf9b7c9952c3239d963ff530d95fd06276a
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-08-22 17:00:02 +0500

    Collapsed run.sh and run_debug.sh into run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 
-uv run \
-fastapi run --entrypoint src.to_do_list_api.main:app
+if [[ DEBUG -eq 1 ]]; then
+    uv run \
+    fastapi dev --entrypoint src.to_do_list_api.main:app
+else
+    uv run \
+    fastapi run --entrypoint src.to_do_list_api.src.main:app
+fi
--- run_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-uv run \
-fastapi dev --entrypoint src.to_do_list_api.main:app
